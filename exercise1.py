263d19a0a66bf9ce24ee7ef9ca95458e133a9e52
ArhamCodes6 <qasarwar@gmail.com>
2025-03-01 12:12:00 +0400

    Coding is cool

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+# Description: This program prints out the words "Coding is cool" using three variables.
+# Word1 is "Coding", word2 is "is", and word3 is "cool".
+word1 = "Coding"
+word2 = "is"
+word3 = "cool"
+print(word1 + " " + word2 + " " + word3)
